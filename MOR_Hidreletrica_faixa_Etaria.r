5a648c38017bdac51b07434d38a837fa6959ae70
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-02-29 16:12:38 -0300

    Update and rename MOR_Hidreletrica_faixa_Eta.r to MOR_Hidreletrica_faixa_Etaria.r

--- MOR_Hidreletrica_faixa_Etaria.r
+++ MOR_Hidreletrica_faixa_Etaria.r
@@ -88,9 +88,9 @@ for(i in 1:length(mencancernames)){
 
 #########################################################################################################################
 
-#calculating for women
+#calculando para as mulheres
 
-#tidying data
+
 copi <- novmort
 copi <- as.data.frame(copi)
 colnames(copi) <- copi[1,]
@@ -98,26 +98,26 @@ copi <- copi[-1,]
 copi <- copi[,-c(1,2,3,4,5,6,7,8,9,10,13,14,15,16,19,20,21,22,23,24,25,26,27,28,29,30,32,33,34,35,36,37)]
 copi <- copi[,-c(2,4,5)]
 copi <- copi[,-c(6,8,4)]
+###########################
 
-#Mor per cancer and sex hydroeletric
+
 novmort<-as.data.frame(novmort)
 copim <- copi %>% filter(SEXO=="F"&NEOPLASIA=="Sim")
-#women
 
-#women age vector
+#vetor de idades das mulheres
 agewomen <- colSums(table(copim$Nova_classificação_diagnóstico_principal.1,copim$`FAIXA ETARIA OBITO`))
 agewomen
 
 cancerwomen<-table(copim$Nova_classificação_diagnóstico_principal.1)#women cancer vector
 
 canceridadewomen<-table(copim$Nova_classificação_diagnóstico_principal.1,copim$`FAIXA ETARIA OBITO`)
-canceridadewomen#n of cancer cases per age
+canceridadewomen#n de casos
 
 canceridadewomen <- as.data.frame(canceridadewomen)
 
-canceridadewomen <- spread(canceridadewomen,key = Var2,value = Freq)#spreading data
+canceridadewomen <- spread(canceridadewomen,key = Var2,value = Freq)
 
-womencancernames <- canceridadewomen[,1]#cancer names vector
+womencancernames <- canceridadewomen[,1]
 
 
 canceridadewomen <- canceridadewomen[,-c(1)]
@@ -125,51 +125,40 @@ womencancernames
 
 
 
-canceridadewomen#mirrowed with womencancer names vector
+canceridadewomen
 
 #table(copim$Nova_classificação_diagnóstico_principal.1)
 
-#algorithm
 
 
 length(cancerwomen)
-#in the following vectors every pos is a age;
-#a=vetor[i]#30 a 39 com doenca	#exposure non healthy#cancer
-#c=ageman[i]-a	#30 a 39 sem deoenca #exposure but healthy#agenam
-#b=vetordecancer[i]-a#que n tem 30 a 39 e tem doenca #non exposure non healthy
-#expon=vetordecancer[i]+c #todas as exposicoes com e sem doenca e nao exposicoes com a doenca all the exposures and non healthy non exposure
-#d=1130-expon#que n tem 30 a 39 e n tem a doenca #non exposure and healthy general population-exposure and non healthy non exposure
 
 listacancerwomen <- list(NULL)
-#filling the list
+
 for(i in 1:length(womencancernames)){
   listacancerwomen[i]=list(NULL)
 }
-names(listacancerwomen)=womencancernames#assigning names
 
+names(listacancerwomen)=womencancernames
 
 for(i in 1:length(womencancernames)){
   for(j in 1:length(agewomen)){
     listacancerwomen[[i]][j]=canceridadewomen[i,j]
     
   }
-}#full women cancer list
+}#lista cancer mulheres
 
 
-#lists down bellow
+#lista mor abaixo
 
-
-#lists down bellow
 listaMORWOMEN=list(NULL)
 for(i in 1:length(womencancernames)){
   listaMORWOMEN[[i]] = list("30 to 39"=list(NULL),"40 to 49"=list(NULL),"50 to 59"=list(NULL),"60 to 69"=list(NULL),"70 to 79"=list(NULL),"80 or more"=list(NULL))
 }
 
-names(listaMORWOMEN)=womencancernames#assigning names
+names(listaMORWOMEN)=womencancernames
 
 
-
-#for women
 for(i in 1:length(listacancerwomen)){
   for(j in 1:length(agewomen)){
     a=listacancerwomen[[i]][[j]]
@@ -180,4 +169,4 @@ for(i in 1:length(listacancerwomen)){
     
     listaMORWOMEN[[i]][[j]]= oddsratio(a,b,c,d,conf.level = 0.95,p.calc.by.independence = T)  
   }
-}
+}#Lista MOR mulheres
