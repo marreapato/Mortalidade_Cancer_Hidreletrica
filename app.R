a88895d2afd65d79c4f957694e8a1edaf2f516f6
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-04-25 17:17:30 -0300

    mor por câncer do brasil entre 1985 ate 20019

---
+++ app.R
@@ -0,0 +1,28 @@
+#shiny app
+
+library(shiny)
+
+#definindo interface
+
+ui <- fluidPage(titlePanel("Painel das chances de mortalidade por câncer no Brasil."),
+                sidebarLayout(sidebarPanel("Sexo"),mainPanel(h2("MOR",align="center"),
+                                                             p("Aplicativo shiny realizado para monitorar a MOR da população brasileira por câncer(calculada no período entre 1985 até 2019)."))))
+
+#definindo a logica do servidor requerida para realizar evento
+
+server <- function(input,output){
+  
+  output$distPlot <- renderPlot({
+    
+    x <- faithful$waiting
+    bins <- seq(min(x),max(x),length.out = input$bins+1)
+    
+    hist(x,breaks = bins,col="red",border="white",
+         xlab="tempo para erupção",
+         ylab="histograma do tempo de espera")
+  })
+  
+}
+
+#rodando app
+shinyApp(ui,server)
