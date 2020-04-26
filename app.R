6020e4193a318b8c6409816d6c8ac078d66994b0
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-04-26 17:38:44 -0300

    Update app.R

--- app.R
+++ app.R
@@ -5,14 +5,18 @@ library(shiny)
 #definindo interface
 
 ui <- fluidPage(titlePanel("Painel das chances de mortalidade por câncer no Brasil."),
-                sidebarLayout(sidebarPanel("Sexo"),mainPanel(h2("MOR",align="center"),
-                                                             p("Aplicativo shiny realizado para monitorar a MOR da população brasileira por câncer(calculada no período entre 1985 até 2019)."))))
+                sidebarLayout(sidebarPanel(helpText(h3("Selecione o sexo da parcela da população brasileira que deseja estudar")),selectInput("sex","Sexo",c("Masculino"="masculino","Feminino"="feminino"))),mainPanel(h2("MOR",align="center"),
+                                                             p("Aplicativo shiny realizado para monitorar a MOR da população brasileira por câncer(calculada no período entre 1985 até 2019)."),textOutput("selected_var"))))
 
 #definindo a logica do servidor requerida para realizar evento
 
-#server <- function(input,output){
+server <- function(input,output){
   
-#}
+  output$selected_var <- renderText(paste("Você esta analizando a população do sexo",input$sex))
+
+}
 
 #rodando app
-#shinyApp(ui,server)
+shinyApp(ui,server)
+
+#?selectInput()
