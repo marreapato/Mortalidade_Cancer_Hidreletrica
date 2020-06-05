5b3a8e21e518b03351fabdebe4504c13e6b454c9
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-06-05 17:17:57 -0300

    Update app.R

--- app.R
+++ app.R
@@ -3,6 +3,8 @@
 library(shiny)
 library(fmsb)
 library(tidyverse)
+install.packages("geobr",dependencies = T)
+library(geobr)
 
 setwd('/home/lucas/Desktop/app_cancer')
 
@@ -165,15 +167,23 @@ for (j in 1:length(regioesm)) {
 #definindo interface
 
 ui <- fluidPage(titlePanel("Painel das chances de mortalidade por câncer no Brasil."),
-                sidebarLayout(sidebarPanel(helpText(h3("Selecione o sexo da parcela da população brasileira que deseja estudar")),selectInput("sex","Sexo",c("Masculino"="masculino","Feminino"="feminino"))),mainPanel(h2("MOR",align="center"),
-                                                                                                                                                                                                                        p("Aplicativo shiny realizado para monitorar a MOR por região da população brasileira por câncer(calculada no período entre 1985 até 2018)."),textOutput("selected_var"))))
+                sidebarLayout(sidebarPanel(helpText(h3("Selecione o sexo da parcela da população brasileira que deseja estudar"))
+                                           ,selectInput("sex","Sexo",c("Masculino"="masculino","Feminino"="feminino")),selectInput("reg","Região",c("Norte"="norte","Nordeste"="nordeste")))
+                              ,mainPanel(h2("MOR",align="center"),
+                                                                                                                                                                                                                      p("Aplicativo shiny realizado para monitorar a MOR por região da população brasileira por câncer(calculada no período entre 1985 até 2018)."),textOutput("selected_var")
+                                                                                                                                                                                                                      )
+                              )
+                )
 
 #definindo a logica do servidor requerida para realizar evento
 
 server <- function(input,output){
   
-  output$selected_var <- renderText(paste("Você esta analizando a população do sexo",input$sex))
   
+  output$selected_var <- renderText(paste("Você esta analizando a população do sexo",input$sex))
+    
+
+    
 }
 
 #rodando app
