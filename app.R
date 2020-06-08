d420dfceb5c08a2bc6d7dd44848d24e3ec15d26c
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-06-08 17:23:57 -0300

    Update app.R

--- app.R
+++ app.R
@@ -168,7 +168,7 @@ ui <- fluidPage(titlePanel("Painel das chances de mortalidade por câncer no Bra
                 sidebarLayout(sidebarPanel(helpText(h3("Selecione o sexo da parcela da população brasileira que deseja estudar"))
                                            ,selectInput("sex","Sexo",c("Masculino"="masculino","Feminino"="feminino")),selectInput("reg","Região",c("Norte"="norte","Nordeste"="nordeste","Sudeste"="sudeste","Sul"="sul","Centro Oeste"="centro oeste")))
                               ,mainPanel(h2("MOR",align="center"),
-                                         p("Aplicativo shiny realizado para monitorar a MOR por região da população brasileira por câncer(calculada no período entre 1985 até 2018)."),textOutput("selected_var"),textOutput("new_selected_var")
+                                         p("Aplicativo shiny realizado para monitorar a MOR por região da população brasileira por câncer(calculada no período entre 1985 até 2018)."),textOutput("selected_var"),textOutput("new_selected_var"),textOutput("mor")
                               )
                 )
 )
@@ -180,7 +180,8 @@ server <- function(input,output){
   
   output$selected_var <- renderText(paste("Você esta analizando a população do sexo",input$sex))
   output$new_selected_var <- renderText(paste("\n Situada na região",input$reg))
-  output$mor <- renderText(paste(""))
+
+  output$mor <- renderText(paste("mor= ",round(mor_regioesh$sudh$`00 a 04`$estimate,2)))
 
 }
 
